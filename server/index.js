ed93bab4421c23dfb351616dce244e8e77814189
Thomas Lowry <minilao94@yahoo.com>
2018-03-24 23:54:32 +0000

    server running

--- server/index.js
+++ server/index.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const app = express();
+app.get('*', (req, res) => {
+    res.status(200).json('You\'ve reached SnapLynx!');
+});
+app.listen(3000, () => console.log('listening on 3000'));
